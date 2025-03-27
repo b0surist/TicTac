2704ad7d3aeecfbb31507d6da48d77656bd3a8e6
2020b-Surányi István <ifjsuranyiistvan@gmail.com>
2025-03-27 18:07:39 +0100

    tictac

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import "bootstrap/dist/css/bootstrap.css"
